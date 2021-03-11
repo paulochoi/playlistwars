5e27f3252858805f3524ec14c396c8b84f75e5be
Paulo Choi <paulochoi@Paulos-MBP.fios-router.home>
2021-03-10 22:56:38 -0500

    Day 1 progress and boilerplate for spotify login

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const queryString = require('querystring');
+const { getRandomString } = require('./utils/auth.js');
 
 const app = express();
 const PORT = '3000';
 
+app.use(cookieParser());
+
 app.get('/', (req, res, next) => {
   res.json({ message: 'YO!' });
+});
+
+app.get('/login', (req, res, next) => {
+  const state = getRandomString();
+  res.cookie('spotify_auth_state', state);
+  const scope =
+    'user-read-currently-playing streaming playlist-modify-public user-read-email';
+
+  res.redirect(
+    `https://accounts.spotify.com/authorize?${queryString.stringify({
+      response_type: 'code',
+      client_id: process.env.CLIENT_ID,
+      scope,
+      redirect_uri: process.env.REDIRECT_URI,
+      state,
+    })}`
+  );
 });
 
 /**

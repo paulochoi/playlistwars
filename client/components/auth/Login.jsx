791cb2b801ba128e3ece73d0f300dfe9136b66df
Paulo Choi <paulochoi@Paulos-MBP.fios-router.home>
2021-03-12 21:44:56 -0500

    Get spotify song to play

--- client/components/auth/Login.jsx
+++ client/components/auth/Login.jsx
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import Button from "react-bootstrap/Button";
 import queryString from "query-string";
+import { setupPlayer } from "../../player/player.js";
 
 const url = "http://localhost:3000";
 const Login = () => {
   const [user, setUser] = useState({});
-  useEffect(() => {
+  useEffect(async () => {
     const parsed = queryString.parse(window.location.search);
     const loginState = parsed.access_token;
     if (loginState) {
+      const deviceID = await setupPlayer(loginState);
+      console.log(deviceID);
+
+      fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceID}`, {
+        method: "PUT",
+        body: JSON.stringify({
+          uris: ["spotify:track:7xGfFoTpQ2E7fRF5lN10tr"],
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${loginState}`,
+        },
+      }).then((a) => console.log(a));
+
       fetch("https://api.spotify.com/v1/me", {
         headers: { Authorization: "Bearer " + loginState },
       })
